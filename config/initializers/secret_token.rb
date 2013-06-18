ea1fde03d9fffb29a46aa52638508734361e03f2
Jonathan Lai <jonathanlai@eltux.net>
2013-06-17 19:04:40 -0700

    scaffolded product and rake db migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zurbstore::Application.config.secret_token = 'ad6d03b09fa93036b02c3bda059c7ef4daabd7200d123e90a704133c78db6cbaffdc37c15c7b409995832a83e7cc550f91aabc656b47655fc32a638a3390c164'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zurbstore::Application.config.session_store :cookie_store, key: '_zurbstore_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zurbstore::Application.config.session_store :active_record_store
